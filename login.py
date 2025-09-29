3c76ee74aa52b7f204a6b4d68ed7b7a1e0c3dad4
Efe ERKEN <efeerkenn@hotmail.com>
2025-09-29 23:02:56 +0200

    Simplify script for headless server and customize
    * Removed type hints
    * Removed typer
    * Fixed response parsing for my use case (YouFirst student dorm Wi-Fi
      with yearly codes)
    * Read Wifirst account credentials from environment variables instead of
      typer
    * Add response parsing to force login even if you're over your allowed
      simultaneously connected device limit (drop the oldest connected
      device)

--- login.py
+++ login.py
@@ -1,6 +1,6 @@
 import requests
 import logging
-import typer
+import os
 
 # first endpoint where you login with wifirst credentials
 LOGIN_PAGE = "https://portal-front.wifirst.net/api/sessions"
@@ -22,21 +22,25 @@ def get_fragment():
     r = requests.get(SETTINGS_PAGE)
     logging.info("Fetched settings page")
     fragment = next(x for x in r.json().get('fragments')
-                    if x['template'] == 'end_user_freezone_signin')
+                    if x['template'] == 'end_user_coupon_signin')
     logging.info(
         f'Extracted fragment with id "{fragment["id"]}" from settings page')
 
     return fragment['id']
 
 
-def get_payload1(email: str, password: str):
+def get_payload1(email, password):
     return {"email": email,
             "password": password,
             "box_token": get_token(),
             "fragment_id": get_fragment()}
+            
+def get_payload1_force(email, password):
+    payload = get_payload1(email, password)
+    payload["force"] = True
+    return payload
 
-
-def get_payload2(response: requests.Response):
+def get_payload2(response):
     return {
         "username": response['radius'].get('login'),
         "password": response['radius'].get('password'),
@@ -46,9 +50,19 @@ def get_payload2(response: requests.Response):
     }
 
 
-def login(email: str, password: str = typer.Option(..., "--password", "-p", prompt=True, hide_input=True)):
+def login(email, password):
     logging.info(f'Logging in as {email}')
-    resp = requests.post(LOGIN_PAGE, get_payload1(email, password)).json()
+    resp = requests.post(LOGIN_PAGE, get_payload1(email, password))
+    status = resp.status_code
+    resp = resp.json()
+    print(resp) # for future me (Efe): this started failing due to Wifirst error saying connected device limit reached (but on the web form this can be bypassed...)
+    if status == 401 and resp["error"]["code"] == "usage_exceeded":
+        isForce = input("You've reached maximum number of connected devices, would you like to force and drop the oldest connected device? ([y]/n) ").strip().lower()
+        if isForce and isForce != "y":
+            logging.error("WiFi login aborted due to too many connected devices on your account")
+            sys.exit(1)
+        # retry the login post request with the "force" flag this time
+        resp = requests.post(LOGIN_PAGE, get_payload1_force(email, password)).json()
     logging.info(
         f'Successfully logged in and got wifirst credentials: {resp["radius"].get("login")} with password {resp["radius"].get("password")}')
     requests.post(HTML_LOGIN_PAGE, get_payload2(resp))
@@ -56,6 +70,7 @@ def login(email: str, password: str = typer.Option(..., "--password", "-p", prom
 
 if __name__ == "__main__":
     try:
-        typer.run(login)
+        login(os.environ["WIFIRST_EMAIL"], os.environ["WIFIRST_PASSWD"])
     except requests.exceptions.ConnectionError:
         logging.error("You are not connected to the wifirst network")
+        sys.exit(1)
